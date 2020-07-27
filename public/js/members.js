6c7a3f8430e2d2fb213fa8d80930a88395eaa2dd
AKKramer <austinkkramer@gmail.com>
2020-07-27 09:58:44 -0500

    Further progress on front end JS

--- public/js/members.js
+++ public/js/members.js
@@ -1,7 +1,19 @@
-$(document).ready(function() {
-    // This file just does a GET request to figure out which user is logged in
-    // and updates the HTML on the page
-    $.get("/api/user_data").then(function(data) {
-      $(".member-name").text(data.email);
+$(document).ready(function () {
+  // This file just does a GET request to figure out which user is logged in
+  // and updates the HTML on the page
+  $.get("/api/user_data").then(function (data) {
+    // "data" pulled from passport only contains email address and password. 
+    $(".member-name").text(data.email);
+    
+    // Using the get request below, uses the id pulled from above, and finds the specific user's data. 
+    $.get(`/api/user/${data.id}`).then(function (data) {
+      console.log('This users data:', data)
+
+
     });
-  });
+
+
+
+  })
+
+})
